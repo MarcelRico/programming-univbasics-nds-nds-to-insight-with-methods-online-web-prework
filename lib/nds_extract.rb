bb4e8c1682e3c746f5496a44773a93fa75209130
Marcel Rico <personal.mr4@gmail.com>
2019-11-30 16:25:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,11 +6,27 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  result = {}
-  nil
+  
+  r_hash = {}
+  
+  nds.length.times do |outer_idx|
+    
+    director_total = 0
+    director = nds[outer_idx][:name]
+    movie_list = nds[outer_idx][:movies]
+    
+    movie_list.length.times do |inner_index|
+      director_total+=movie_list[inner_index][:worldwide_gross]
+    end
+    
+    r_hash[director] = director_total
+  end
+  
+  r_hash
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  puts directors_totals(director_data)
 end
