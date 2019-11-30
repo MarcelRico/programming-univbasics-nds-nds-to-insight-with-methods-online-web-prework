0a1e63ebf7ebc6a58a2d5639df79275d8f0319f6
Marcel Rico <personal.mr4@gmail.com>
2019-11-30 16:27:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,5 +28,5 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  puts directors_totals(director_data)
+  return directors_totals(director_data)[0].value
 end
